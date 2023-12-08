9a182d647c06e70ae8b4caac0b95a9b15af66fb6
Nat Peterson <natpeterson@gmail.com>
2023-12-08 07:33:41 -0800

    day 8.2

--- src/day08/day08.py
+++ src/day08/day08.py
@@ -12,13 +12,14 @@ Part 2:
 import logging
 import time
 import aoc_common.aoc_commons as ac
+from math import gcd
 
 YEAR = 2023
 DAY = 8
 
 locations = ac.get_locations(__file__)
 logger = ac.retrieve_console_logger(locations.script_name)
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
 # td.setup_file_logging(logger, locations.output_dir)
 try:
     ac.write_puzzle_input_file(YEAR, DAY, locations)
@@ -26,23 +27,7 @@ except ValueError as e:
     logger.error(e)
 
 
-def main():
-    puzzle = locations.input_file
-    # puzzle = locations.sample_input_file
-    with open(puzzle, mode="rt") as f:
-        data = f.read().splitlines()
-
-    # logger.debug(data)
-    steps = list(data[0])
-    logger.debug(steps)
-    assert data[1] == ""
-    junctions = {}
-    for row in data[2:]:
-        left, right = row.split(" = ")
-        right = right[1:-1].split(", ")
-
-        junctions[left] = right
-    logger.debug(junctions)
+def part1(steps, junctions):
     current = "AAA"
     step_idx = 0
     followed = [current]
@@ -62,7 +47,83 @@ def main():
         followed.append(current)
 
     assert current == "ZZZ"
-    logger.debug("Part 1: %d", len(followed))
+    logger.info("Part 1: %d", len(followed))
+
+
+def explore_from_a_start(start, steps, junctions, ends_in_Z):
+    # how long until we get back to ZZZ?
+    current = start
+    step_idx = 0
+    step_count = 0
+    visits = []
+    while len(visits) < 5:
+        if steps[step_idx] == "R":
+            current = junctions[current][1]
+        elif steps[step_idx] == "L":
+            current = junctions[current][0]
+        else:
+            raise ValueError(f"Unknown step {steps[step_idx]}")
+        step_idx += 1
+        step_idx %= len(steps)
+        step_count += 1
+        if current in ends_in_Z:
+            # logger.debug("ZZZ at step %d", step_count)
+            visits.append(step_count)
+
+    return visits
+
+
+def main():
+    puzzle = locations.input_file
+    # puzzle = locations.sample_input_file
+    with open(puzzle, mode="rt") as f:
+        data = f.read().splitlines()
+
+    # logger.debug(data)
+    steps = list(data[0])
+    logger.debug(steps)
+    assert data[1] == ""
+    junctions = {}
+    for row in data[2:]:
+        left, right = row.split(" = ")
+        right = right[1:-1].split(", ")
+
+        junctions[left] = right
+    logger.debug(junctions)
+    part1(steps, junctions)
+
+    # Part 2
+    ends_in_A = [loc for loc in junctions if loc.endswith("A")]
+    ends_in_Z = [loc for loc in junctions if loc.endswith("Z")]
+    logger.debug(ends_in_A)
+    logger.debug(ends_in_Z)
+
+    periods = []
+    for start in ends_in_A:
+        logger.debug(f"Start at {start}")
+        visits = explore_from_a_start(start, steps, junctions, ends_in_Z)
+        logger.debug(visits)
+        # find period of visits
+        period = visits[1] - visits[0]
+        assert visits[2] - visits[1] == period
+        assert visits[3] - visits[2] == period
+        assert visits[4] - visits[3] == period
+        periods.append(period)
+    # find the LCM of the periods
+    lcm = 1
+    for period in periods:
+        lcm = lcm * period // gcd(lcm, period)
+    logger.info("Part 2: %d", lcm)
+
+
+def gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
+
+def lcm(a, b):
+    return (a * b) // gcd(a, b)
 
 
 if __name__ == "__main__":
