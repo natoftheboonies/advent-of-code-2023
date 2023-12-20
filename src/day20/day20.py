87f580095eecd4ff173f2db8f8f40b7cfc42606b
Nat Peterson <natpeterson@gmail.com>
2023-12-20 10:32:41 -0800

    day20 part 1

--- src/day20/day20.py
+++ src/day20/day20.py
@@ -27,36 +27,106 @@ except ValueError as e:
     logger.error(e)
 
 
+def run_queue(button_press, modules):
+    low_pulses = 0
+    high_pulses = 0
+    queue = deque([button_press])  # (module, signal, source)
+    logger.debug(f"{queue}")
+    while queue:
+        name, signal, source = queue.popleft()
+        # TODO ai below
+        logger.debug(f"queue: {source} -{'high' if signal else 'low'}-> {name}")
+        if signal == 0:
+            low_pulses += 1
+        else:
+            assert signal == 1
+            high_pulses += 1
+        module = modules[name]
+        if module["type"] == "broadcast":
+            # broadcast to all
+            for notify in module["notify"]:
+                queue.append((notify, signal, name))
+        elif module["type"] == "%":
+            # flip-flop ignores high pulses
+            if signal == 1:
+                continue
+            # otherwise flip and notify
+            module["state"] += 1
+            module["state"] %= 2
+            for notify in module["notify"]:
+                queue.append((notify, module["state"], name))
+        elif module["type"] == "&":
+            # remember last pulse for each input
+            assert source in module["received"]
+            module["received"][source] = signal
+            send = 1
+            if all(sig == 1 for sig in module["received"].values()):
+                # all inputs are high
+                send = 0
+            for notify in module["notify"]:
+                queue.append((notify, send, name))
+        elif module["type"] is None:
+            continue
+        else:
+            assert False
+    return low_pulses, high_pulses
+
+
 def main():
     puzzle = locations.input_file
-    puzzle = locations.sample_input_file
+    # puzzle = locations.sample_input_file
     with open(puzzle, mode="rt") as f:
         data = f.read().splitlines()
 
     logger.debug(data)
     modules = {}
-    broadcast = []
+    all_notify = set()
     for line in data:
         left, right = line.split(" -> ")
-        logger.debug(f"{left} -> {right}")
+        # logger.debug(f"{left} -> {right}")
         if "," in right:
             right = right.split(", ")
-        if left[0] in "&%":
-            left = (left[0], left[1:])
-            modules[left[1]] = (left[0], right)
+
+        if left[0] == "%":
+            name = left[1:]
+            state = 0
+            notify = right if isinstance(right, list) else [right]
+            modules[name] = {"type": "%", "state": state, "notify": notify}
+        elif left[0] == "&":
+            name = left[1:]
+            received = {}
+            notify = right if isinstance(right, list) else [right]
+            modules[name] = {"type": "&", "received": received, "notify": notify}
         else:
             assert left == "broadcaster"
-            broadcast = right
-    logger.debug(f"{modules.keys()}")
+            name = left
+            modules[left] = {"type": "broadcast", "notify": right}
+        all_notify.update(modules[name]["notify"])
 
-    low_pulses = 1  # button to broadcaster
-    high_pulses = 0
+    # add untyped
+    for name in all_notify:
+        logger.debug(f"{name}")
+        if name not in modules:
+            modules[name] = {"type": None, "notify": []}
+
+    for name, module in modules.items():
+        for notify in module["notify"]:
+            if modules[notify]["type"] == "&":
+                modules[notify]["received"][name] = 0
+    # fi
+    for module in modules.values():
+        logger.debug(f"{module}")
     # button sends to broadcaster
-    queue = deque([])
-    for module in broadcast:
-        low_pulses += 1
-        queue.append(modules[module])
-    logger.debug(f"{queue}")
+    low_pulses = 0
+    high_pulses = 0
+    button_press = ("broadcaster", 0, "button")
+    for _ in range(1000):
+        low, high = run_queue(button_press, modules)
+        low_pulses += low
+        high_pulses += high
+    logger.info(f"low pulses: {low_pulses}")
+    logger.info(f"high pulses: {high_pulses}")
+    logger.info("Part 1: %d", low_pulses * high_pulses)
 
 
 if __name__ == "__main__":
